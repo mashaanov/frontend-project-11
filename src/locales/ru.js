7a1402defa39bd950b4b060e5456e3a44a70e684
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-09-12 17:09:50 +0400

    add ru.js

---
+++ src/locales/ru.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    feedbacks: {
+      feedbackSuccess: 'RSS успешно загружен',
+      feedbackWrongUrl: 'Ссылка должна быть валидным URL',
+      feedbackRepeat: 'RSS уже существует',
+      feedbackNoRSS: 'Ресурс не содержит валидный RSS',
+      feedbackEmpty: 'Не должно быть пустым',
+    },
+    posts: 'Посты',
+    feeds: 'Фиды',
+    buttons: {
+      addBtn: 'Добавить',
+      viewBtn: 'Просмотр',
+    },
+    errors: {
+      network: 'Ошибка сети',
+    },
+  },
+};
