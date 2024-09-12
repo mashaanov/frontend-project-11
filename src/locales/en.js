f89f18d5ff8ca9679708920aa0a047a5e1eac423
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-09-12 17:01:03 +0400

    add en.js

---
+++ src/locales/en.js
@@ -0,0 +1,20 @@
+export default {
+  translation: {
+    feedbacks: {
+      feedbackSuccess: 'RSS successfully loaded',
+      feedbackWrongUrl: 'Link must be valid URL',
+      feedbackRepeat: 'RSS already exists',
+      feedbackNoRSS: 'RSS doesn\'t contain valid RSS',
+      feedbackEmpty: 'Must not be empty',
+    },
+    posts: 'Posts',
+    feeds: 'Feeds',
+    buttons: {
+      addBtn: 'Add',
+      viewBtn: 'View',
+    },
+    errors: {
+      network: 'Network error',
+    },
+  },
+};
