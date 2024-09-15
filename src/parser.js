b6a9c3ba94b4d4f3ca85617756a2b8c314f882b3
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-09-15 13:05:52 +0400

    parser.js

---
+++ src/parser.js
@@ -0,0 +1,30 @@
+export default (responseData) => {
+  const parser = new DOMParser();
+  const xmlDocument = parser.parseFromString(responseData, 'text/xml');
+
+  if (xmlDocument.querySelector('parsererror')) {
+    const err = new Error();
+    err.name = 'parsingError';
+    throw err;
+  } else {
+    const channel = xmlDocument.querySelector('channel');
+    const channelTitle = channel.querySelector('title').textContent;
+    const channelDescription = channel.querySelector('description').textContent;
+    const feed = { channelTitle, channelDescription };
+
+    const itemsElements = document.querySelector('items');
+    const posts = [...itemsElements].map((item) => {
+      const description = item.querySelector('description').textContent;
+      const title = item.querySelector('title').textContent;
+      const link = item.querySelector('link').textContent;
+      return {
+        title,
+        description,
+        link,
+      };
+    });
+
+    const parsedRSS = { feed, posts };
+    return parsedRSS;
+  }
+};
