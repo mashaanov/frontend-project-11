80d5a1c035896e601ec3e3271c9ea1461bb5d02f
Maria Novikova <m.v.novikovaaa@gmail.com>
2024-09-27 18:06:00 +0400

    update parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,30 +1,21 @@
-export default (responseData) => {
+export default (response) => {
   const parser = new DOMParser();
-  const xmlDocument = parser.parseFromString(responseData, 'application/xml');
+  const content = parser.parseFromString(response, 'text/xml');
+  const error = content.querySelector('parsererror');
 
-  if (xmlDocument.querySelector('parsererror')) {
-    const err = new Error();
-    err.name = 'parsingError';
-    throw err;
-  } else {
-    const channel = xmlDocument.querySelector('channel');
-    const channelTitle = channel.querySelector('title').textContent;
-    const channelDescription = channel.querySelector('description').textContent;
-    const feed = { channelTitle, channelDescription };
-
-    const itemsElements = document.querySelector('items');
-    const posts = [...itemsElements].map((item) => {
-      const description = item.querySelector('description').textContent;
-      const title = item.querySelector('title').textContent;
-      const link = item.querySelector('link').textContent;
-      return {
-        title,
-        description,
-        link,
-      };
-    });
-
-    const parsedRSS = { feed, posts };
-    return parsedRSS;
+  if (error) {
+    const parserError = new Error(error.textContent);
+    parserError.name = 'parserError';
+    throw parserError;
   }
+
+  return {
+    title: content.querySelector('title').textContent,
+    description: content.querySelector('description').textContent,
+    posts: [...content.querySelectorAll('item')].map((post) => ({
+      title: post.querySelector('title').textContent,
+      description: post.querySelector('description').textContent,
+      link: post.querySelector('link').textContent,
+    })),
+  };
 };
